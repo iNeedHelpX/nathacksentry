86bc50190dd02b1b8f09a5d89ec50c24fcdd9c3b
Julia Pak <jpak0444@gmail.com>
2022-07-30 20:24:18 -0400

    ok

---
+++ lib/start/start.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//the start of the app.
+class Start extends StatefulWidget {
+  Start({Key? key}) : super(key: key);
+
+  @override
+  State<Start> createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+
+        //replace this with a call to a switch statement to check for login state
+        child:,
+      ),
+
+      //genrates the routes for the app
+      // onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
