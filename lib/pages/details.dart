aa8f05cdf46154c9f67a954f89c0036a28658dc5
Julia Pak <jpak0444@gmail.com>
2022-08-05 14:05:07 -0400

    ok

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:nathacksentry/colors/colours_list.dart';
+
 import 'package:nathacksentry/controller/second_ratcontroller.dart';
-import 'package:nathacksentry/globalvars.dart';
 
 class RatsDetail extends StatelessWidget {
+  // final String imgUrl;
+  final RatsControl ratsControl = Get.find();
+  final int rank;
+
+  final String imgUrl;
   RatsDetail({
     Key? key,
     required this.imgUrl,
     required this.rank,
   }) : super(key: key);
-
-  final String imgUrl;
-  final int rank;
-  // final String imgUrl;
-  final RatsControl ratsControl = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +22,34 @@ class RatsDetail extends StatelessWidget {
     return Container(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              Center(
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      height: 500,
-                      child:
-                          Image(image: NetworkImage(imgUrl), fit: BoxFit.cover),
-                    ),
-                    SafeArea(
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Icon(
-                          Icons.backspace,
-                          color: Color.fromARGB(255, 247, 0, 97),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 500,
+                        child: Image(
+                            image: NetworkImage(imgUrl), fit: BoxFit.cover),
+                      ),
+                      SafeArea(
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.backspace,
+                            color: Color.fromARGB(255, 247, 0, 97),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
